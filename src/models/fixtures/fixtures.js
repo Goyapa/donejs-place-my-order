06ce475ad54ffd68f46dcfee149247b453b170eb
Michael Macherey <macherey@gmx.net>
2015-11-16 01:33:34 +0100

    Creating a unit-tested view model

--- src/models/fixtures/fixtures.js
+++ src/models/fixtures/fixtures.js
@@ -2,3 +2,7 @@ import fixture from 'can-fixture';
 
 
 import 'place-my-order/models/fixtures/restaurant';
+
+import 'place-my-order/models/fixtures/state';
+
+import 'place-my-order/models/fixtures/city';
